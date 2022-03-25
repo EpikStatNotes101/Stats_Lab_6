d99590bfb0767e507e1b0aa9c6d6d3a9c4494132
Sonali Pal <sonali@codelogicx.com>
2022-03-25 10:36:14 +0530

    MA 7 days for trend

--- TimeSeries2.R
+++ TimeSeries2.R
@@ -15,3 +15,17 @@ dat_m["year_"] <- dat_m$Year %>% substr(1,4) %>% as.numeric()
 #plot the raw data 
 plot.default(dat_m$year_, dat_m$Area, type = "l")
 
+#ma 7 day (trend)
+for (i in 1:(nrow(dat_m)-6))
+{
+  print(i)
+  
+  print(i+6)
+  dat_m[i+6, "MA_7"] = mean(dat_m$Area[i:(i+6)])
+}
+
+View(dat_m)
+#plot trend
+
+plot.default(dat_m$year_, dat_m$MA_7, type = "l")
+
