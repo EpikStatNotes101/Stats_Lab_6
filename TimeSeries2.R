8a1b8516a717f476b4693c88ca48a12cace66a06
Sonali Pal <sonali@codelogicx.com>
2022-04-01 10:39:35 +0530

    Detrended

--- TimeSeries2.R
+++ TimeSeries2.R
@@ -8,9 +8,10 @@ getwd()
 dat <- read_excel("./dataset_2/DATA _ASSINGNMENT 2.xlsx")
 
 dat
-dat_m = dat[c("Year", "Area")]
 
 dat_m["year_"] <- dat_m$Year %>% substr(1,4) %>% as.numeric()
+dat_m = dat_m[c("year_", "Area")]
+
 
 #plot the raw data 
 plot.default(dat_m$year_, dat_m$Area, type = "l")
@@ -21,11 +22,15 @@ for (i in 1:(nrow(dat_m)-6))
   print(i)
   
   print(i+6)
-  dat_m[i+6, "MA_7"] = mean(dat_m$Area[i:(i+6)])
+  dat_m[i+6, "MA_7"] <- mean(dat_m$Area[i:(i+6)])
 }
-
+row.names(dat_m) <- dat_m$year_
 View(dat_m)
 #plot trend
 
 plot.default(dat_m$year_, dat_m$MA_7, type = "l")
 
+#detrend
+dat_m["detrended"] = dat_m$Area / dat_m$MA_7
+
+plot.default(dat_m$year_, dat_m$detrended, type = "l")
